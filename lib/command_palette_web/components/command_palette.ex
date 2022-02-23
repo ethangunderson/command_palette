51e9da3ccbaad1d26596e4d8af109cdee6715488
Ethan Gunderson <ethan@ethangunderson.com>
2022-02-23 15:23:40 -0600

    replace static results with liveview provided list

--- lib/command_palette_web/components/command_palette.ex
+++ lib/command_palette_web/components/command_palette.ex
@@ -1,6 +1,10 @@
 defmodule CommandPalette.Components.CommandPalette do
   use Phoenix.LiveComponent
 
+  def mount(socket) do
+    {:ok, assign(socket, :results, ["No results"] |> Jason.encode!())}
+  end
+
   def render(assigns) do
     ~H"""
       <div 
@@ -14,20 +18,63 @@ defmodule CommandPalette.Components.CommandPalette do
           <div class="border-b-2 pb-4">
             <input 
                x-trap="open"
-               @keydown.arrow-down.prevent="currentPosition++"
-               @keydown.arrow-up.prevent="currentPosition--"
+               x-on:keydown.arrow-down.prevent="currentPosition++"
+               x-on:keydown.arrow-up.prevent="currentPosition--"
+               phx-keyup="typetype"
+               phx-target={@myself}
+               phx-debounce="200"
                placeholder="Search..." 
                class="text-2xl w-full focus:outline-none"/>
           </div>
         <ul class="max-h-72 overflow-y-auto py-2 text-gray-800">
-          <template x-for="(key, index) in ['result 1', 'result 2', 'result 3', 'result 4']" :key="index">
-            <li x-text="key" 
+    <template x-for={"(key, index) in #{@results}"} :key="index">
+            <li x-text="key"
               :class="{ 'bg-gray-200': index === currentPosition }"
-              class="select-none px-4 py-2 hover:bg-gray-200 hover:cursor-pointer"></li>
+              class="select-none px-4 py-2 hover:bg-gray-200 hover:cursor-pointer">
+            </li>
           </template>
         </ul>
       </div>
     </div> 
     """
+  end
+
+  def result_list(assigns) do
+    ~H"""
+      <%= for result <- @results do %>
+        <li>
+          <%= IO.inspect(result) %>
+        </li>
+      <% end %>
+    """
+  end
+
+  def handle_event("typetype", %{"value" => ""}, socket) do
+    {:noreply, assign(socket, :results, ["No results"] |> Jason.encode!())}
+  end
+
+  def handle_event("typetype", %{"value" => search_string}, socket) do
+    possible_results = [
+      "Result 1",
+      "Result 2",
+      "Result 3",
+      "Result 4",
+      "Result 5",
+      "Result 6",
+      "Result a",
+      "Result b",
+      "Result c",
+      "Result d"
+    ]
+
+    results =
+      Enum.filter(possible_results, fn result -> String.contains?(result, search_string) end)
+      |> Jason.encode!()
+
+    socket =
+      socket
+      |> assign(:results, results)
+
+    {:noreply, socket}
   end
 end
